63311ff5c9702601f68993503b1fb9cdc0cbf2e9
한용길 <hanyg5678@naver.com>
2022-01-06 17:51:29 +0900

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "2.2.0"
   # insert required variables here
-  bucket_prefix = "hanyg"
+  bucket_prefix = var.prefix
 }
